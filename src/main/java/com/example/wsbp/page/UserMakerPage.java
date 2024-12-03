1ac43b292e3e8006d27ee4e7c2983918dc3bb4d9
sasaki-kentatata <kentatta03@gmail.com>
2024-12-03 21:29:40 +0900

    ユーザー追加2ã2まで2まで行った

--- src/main/java/com/example/wsbp/page/UserMakerPage.java
+++ src/main/java/com/example/wsbp/page/UserMakerPage.java
@@ -1,12 +1,52 @@
 package com.example.wsbp.page;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.wicketstuff.annotation.mount.MountPath;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
+
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import com.example.wsbp.service.IUserService;
 
 @MountPath("UserMaker")
 public class UserMakerPage extends WebPage {
 
+    //IUserService を IoC/DI する
+    @SpringBean
+    private IUserService userService;
+
     public UserMakerPage() {
+        var userNameModel = Model.of("");
+        var userPassModel = Model.of("");
+        // 前回の課題の部分
+        var toHomeLink = new BookmarkablePageLink<>("toHome", HomePage.class);
+        add(toHomeLink);
+
+        //配置したFormコンポーネントを匿名クラス化して処理を上書きする
+        var userInfoForm = new Form<>("userInfo") {
+            @Override
+            protected void onSubmit() {
+                var userName = userNameModel.getObject();
+                var userPass = userPassModel.getObject();
+                var msg = "送信データ："
+                        + userName
+                        + ","
+                        + userPass;
+                System.out.println(msg);
+                userService.registerUser(userName, userPass);
+                setResponsePage(new UserMakerCompPage(userNameModel));
+            }
+        };
+        add(userInfoForm);
+
+        var userNameField = new TextField<>("userName", userNameModel);
+        userInfoForm.add(userNameField);
+
+        var userPassField = new PasswordTextField("userPass", userPassModel);
+        userInfoForm.add(userPassField);
 
     }
 
---
+++ src/main/java/com/example/wsbp/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.example.wsbp.service;
+
+public interface IUserService {
+
+    public void registerUser(String userName, String userPass);
+
+}
